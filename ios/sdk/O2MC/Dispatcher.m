25a0d99b8663eed6e4d9fa215f1d51a9f21cc493
Tim Slot <tim.slot@gmail.com>
2018-07-03 14:21:18 +0200

    remove ip property on iOS

--- ios/sdk/O2MC/Dispatcher.m
+++ ios/sdk/O2MC/Dispatcher.m
@@ -26,7 +26,6 @@
 
     NSMutableDictionary* data = [NSMutableDictionary new];
     [data setObject:_appName forKey:@"AppId"];
-    [data setObject:[self getIPAddress] forKey:@"ip"];
     [data setObject:@"3G" forKey:@"connection"];
     [data setObject:[[UIDevice currentDevice] systemName] forKey:@"os"];
     [data setObject:[[UIDevice currentDevice] systemVersion] forKey:@"osVersion"];
@@ -71,35 +70,6 @@
 
 }
 
-
-- (NSString *)getIPAddress {
-    NSString *address = @"error";
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    int success = 0;
-    // retrieve the current interfaces - returns 0 on success
-    success = getifaddrs(&interfaces);
-    if (success == 0) {
-        // Loop through linked list of interfaces
-        temp_addr = interfaces;
-        while (temp_addr != NULL) {
-            if (temp_addr->ifa_addr->sa_family == AF_INET) {
-                // Check if interface is en0 which is the wifi connection on the iPhone
-                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
-                    // Get NSString from C String
-                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *) temp_addr->ifa_addr)->sin_addr)];
-
-                }
-
-            }
-
-            temp_addr = temp_addr->ifa_next;
-        }
-    }
-    // Free memory
-    freeifaddrs(interfaces);
-    return address;
-}
 
 
 @end
