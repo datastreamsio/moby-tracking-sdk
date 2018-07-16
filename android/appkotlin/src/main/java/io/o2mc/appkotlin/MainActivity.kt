2027ab24757bbcec16c02ca766f1e796cf8a4c34
Edwin van Rooij <edwinvrooij@gmail.com>
2018-07-16 16:59:49 +0200

    hello world from kotlin complete, not using SDK yet

---
+++ android/appkotlin/src/main/java/io/o2mc/appkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.o2mc.appkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
