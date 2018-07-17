ca067e67ecc2614b4b857db3c246defb21a0c7ca
Edwin van Rooij <edwinvrooij@gmail.com>
2018-07-17 11:03:58 +0200

    migrate batch from java to kotlin

---
+++ android/sdk/src/main/java/io/o2mc/sdk/domain/Batch.kt
@@ -0,0 +1,13 @@
+package io.o2mc.sdk.domain
+
+/**
+ * Represents a batch of Events.
+ * Essentially contains events and meta data for analytical purposes.
+ */
+data class Batch(
+  val deviceInformation: DeviceInformation, // final (provides only a getter automatically)
+  val timestamp: String, // final
+  val events: List<Event>, // final
+  val number: Int, // final
+  var retries: Int // not final (additionally provides a setter automatically)
+)
