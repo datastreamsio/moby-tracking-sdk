1b4a8298c98d9608e52576b1e62e9b81af162467
Edwin van Rooij <edwinvrooij@gmail.com>
2018-07-17 11:36:21 +0200

    fix non null value bug

--- android/sdk/src/main/java/io/o2mc/sdk/domain/Event.kt
+++ android/sdk/src/main/java/io/o2mc/sdk/domain/Event.kt
@@ -5,6 +5,6 @@ package io.o2mc.sdk.domain
  */
 data class Event(
   val name: String,
-  val value: Any,
+  val value: Any?, // value can be null when tracking without properties
   val timestamp: String
 )
