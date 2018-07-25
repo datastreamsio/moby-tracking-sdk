0a928b6c9216340cdd6ed459d2edf850df3e7f76
Tim Slot <tim.slot@gmail.com>
2018-07-25 16:55:30 +0200

    add o2mlogger stub files

---
+++ ios/sdk/O2MC/O2MLogger.h
@@ -0,0 +1,18 @@
+//
+//  O2MLogger.h
+//  O2MC
+//
+//  Created by Tim Slot on 25/07/2018.
+//  Copyright © 2018 Adversitement. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface O2MLogger : NSObject
+
++(void) log;
++(void) logI;
++(void) logD;
++(void) logE;
++(void) logF;
+
+@end
---
+++ ios/sdk/O2MC/O2MLogger.m
@@ -0,0 +1,33 @@
+//
+//  O2MLogger.m
+//  datastreams
+//
+//  Created by Tim Slot on 25/07/2018.
+//  Copyright © 2018 Adversitement. All rights reserved.
+//
+
+#import "O2MLogger.h"
+
+@implementation O2MLogger
+
++(void) log; {
+    
+}
+
++(void) logI; {
+    
+}
+
++(void) logD; {
+    
+}
+
++(void) logE; {
+    
+}
+
++(void) logF; {
+    
+}
+
+@end
